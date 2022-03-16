92d5cdcec4e6610374a41602bc485e1d8409d05f
Pac <debianunix6@gmail.com>
2022-03-16 11:58:43 +0100

    transfert

--- src/main/java/com/example/test_3d/MainApplication.java
+++ src/main/java/com/example/test_3d/MainApplication.java
@@ -10,16 +10,20 @@ import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.scene.robot.Robot;
+import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class MainApplication extends Application {
     public PerspectiveCamera camera = new PerspectiveCamera();
-    public double xAxe = 0.0, yAxe = 0.0, zAxe = 0.0, xCamera = 0, yCamera = 0, zCamera = 0.0;
+    public double xAxe = 400.0, yAxe = 0.0, zAxe = 0.0, xCamera = 0, yCamera = 0, zCamera = 0.0, old_xCamera = 0, old_yCamera = 0, old_zCamera = 0.0;
     public BooleanProperty z_Press = new SimpleBooleanProperty(false), q_Press = new SimpleBooleanProperty(false), d_Press = new SimpleBooleanProperty(false), s_Press = new SimpleBooleanProperty(false), shift_Press = new SimpleBooleanProperty(false);
     public BooleanBinding z_and_q = z_Press.and(q_Press), z_and_d = z_Press.and(d_Press), s_and_q = s_Press.and(q_Press), s_and_d = s_Press.and(d_Press);
     public Robot robot = new Robot();
+    public float sensitivity = 0.01f;
+
+    public Rotate cameraRotateX = new Rotate(0, Rotate.X_AXIS), cameraRotateY = new Rotate(0, Rotate.Y_AXIS);
 
     public static void main(String[] args) {
         launch();
@@ -33,6 +37,12 @@ public class MainApplication extends Application {
         camera.setTranslateX(xCamera);
         camera.setTranslateY(yCamera);
         camera.setTranslateZ(zCamera);
+
+        camera.setFieldOfView(70);
+        camera.setVerticalFieldOfView(false);
+        camera.setFarClip(10000);
+
+        camera.getTransforms().addAll(cameraRotateX, cameraRotateY);
 
         scene.setFill(Color.BLUE);
 
@@ -146,15 +156,32 @@ public class MainApplication extends Application {
         });
 
         scene.setOnMouseMoved(event -> {
+
+            old_xCamera = xCamera;
+            old_yCamera = yCamera;
+
             xCamera = event.getX();
+            yCamera = event.getY();
+
+            double delta_x = xCamera - old_xCamera;
+            double delta_y = yCamera - old_yCamera;
+
+            cameraRotateY.setAngle(cameraRotateY.getAngle() + delta_y * sensitivity);
+
+            cameraRotateX.setAngle(cameraRotateX.getAngle() + delta_x * sensitivity);
+
+
+
+            /*xCamera = event.getX();
             yCamera = event.getY();
             double delta_x = event.getSceneX() - xCamera;
             double delta_y = event.getSceneY() - yCamera;
 
             camera.setTranslateX(camera.getTranslateX() - delta_x * 0.05);
             camera.setTranslateY(camera.getTranslateY() - delta_y * 0.05);
+            */
+            //robot.mouseMove(scene.getHeight() / 2, scene.getWidth() / 2);
 
-            robot.mouseMove(scene.getHeight() / 2, scene.getWidth() / 2);
         });
 
         stage.setFullScreen(true);
--- src/main/java/com/example/test_3d/MainController.java
+++ src/main/java/com/example/test_3d/MainController.java
@@ -7,8 +7,6 @@ import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.Box;
 import javafx.scene.shape.CullFace;
-import javafx.scene.shape.Cylinder;
-import javafx.scene.shape.Sphere;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -16,6 +14,8 @@ import java.util.ResourceBundle;
 public class MainController implements Initializable {
     public AnchorPane anchor_main;
     public AmbientLight ambient_light;
+    public int chunk_size = 16, chunck_number = 10, block_size = 50, pos_x = 0, pos_z = 100, pos_y = 600, thickness = 1;
+    public Color box_color;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -23,44 +23,66 @@ public class MainController implements Initializable {
 
         ambient_light.setColor(Color.YELLOW);
 
-
-        Box plateform = new Box(1000, 50, 1000);
-        Cylinder cylinder = new Cylinder(50, 100);
-        Sphere sphere = new Sphere(50);
-
-        cylinder.setCullFace(CullFace.NONE);
-        sphere.setCullFace(CullFace.NONE);
-        plateform.setCullFace(CullFace.NONE);
-
-        PhongMaterial sphere_mat = new PhongMaterial();
-        PhongMaterial cylinder_mat = new PhongMaterial();
-        PhongMaterial platform_mat = new PhongMaterial();
-
-        platform_mat.setDiffuseColor(Color.DARKGREEN);
-        cylinder_mat.setDiffuseColor(Color.RED);
-        sphere_mat.setDiffuseColor(Color.BLACK);
-
-
-        sphere.setMaterial(sphere_mat);
-        cylinder.setMaterial(cylinder_mat);
-        plateform.setMaterial(platform_mat);
+        for (int y = 0; y < thickness; y++) {
+            if (y < 3) {
+                box_color = Color.DARKGREEN;
+            } else {
+                box_color = Color.BROWN;
+            }
+            for (int x = 0; x < chunck_number; x++) {
+                for (int z = 0; z < chunck_number; z++) {
+                    create_chunck(box_color, chunk_size, block_size, pos_x, pos_y, pos_z);
+                    pos_x += block_size * chunk_size;
+                }
+                pos_x = 0;
+                pos_z += block_size * chunk_size;
+            }
+            pos_x = 0;
+            pos_z = 100;
+            pos_y += block_size;
+        }
 
 
-        plateform.setTranslateX(400);
-        plateform.setTranslateY(600);
-        plateform.setTranslateZ(0);
+    }
 
-        cylinder.setTranslateX(200);
-        cylinder.setTranslateY(550);
-        cylinder.setTranslateZ(0);
+    public void create_chunck(Color box_color, int chunk_size, int block_size, int pos_x, int pos_y, int pos_z) {
+        int default_pos_z = pos_z;
+        for (int col = 0; col < chunk_size; col++) {
+            for (int row = 0; row < chunk_size; row++) {
+                Box box = new Box(block_size, block_size, block_size);
+                box.setCullFace(CullFace.NONE);
+                //box.setDrawMode(DrawMode.LINE);
+                PhongMaterial box_mat = new PhongMaterial();
 
-        sphere.setTranslateX(500);
-        sphere.setTranslateY(550);
-        sphere.setTranslateZ(0);
+                box_mat.setDiffuseColor(box_color);
+                box_mat.setSpecularColor(box_color.darker());
 
+                box.setMaterial(box_mat);
 
-        anchor_main.getChildren().add(plateform);
-        anchor_main.getChildren().add(cylinder);
-        anchor_main.getChildren().add(sphere);
+                box.setTranslateX(pos_x);
+                box.setTranslateZ(pos_z);
+                box.setTranslateY(pos_y);
+
+                pos_z += 50;
+
+                anchor_main.getChildren().add(box);
+            }
+            pos_z = default_pos_z;
+            pos_x += 50;
+        }
+    }
+
+    public double lerp(double a0, double a1, double weight) {
+        return (1.0 - weight) * a0 + weight * a1;
+    }
+
+    public void perlin_noise(int x, int y) {
+        int x0 = x;
+        int x1 = x + 1;
+
+        int y0 = y;
+        int y1 = y + 1;
+
+        //return lerp();
     }
 }
